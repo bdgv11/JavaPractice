3d8d2e7a36c61c3e7eedd375b0615f203bc18b34
Bryan Guzman <bdgv11@gmail.com>
2022-10-24 21:14:11 -0600

    Challenge #38 completed.

--- src/Challenges/Challenge_38.java
+++ src/Challenges/Challenge_38.java
@@ -10,8 +10,27 @@ package Challenges;
  *
  */
 
- // TODO complete this!
-
 public class Challenge_38 {
-    
+    public static void main(String[] args) {
+        challenge38("111110111");
+        challenge38("1010101011");
+        challenge38("1111111");
+        challenge38("00001111");
+    }
+
+    public static void challenge38(String binaryNumber) {
+
+        int suma = 0;
+        int pot = binaryNumber.length()-1;
+
+        for (int i = 0; i < binaryNumber.length(); i++) {
+
+            Character binaryValue = binaryNumber.charAt(i);
+            int potencia = (int) Math.pow(2, pot);
+            suma = suma + ( Integer.parseInt(binaryValue.toString())  * potencia);
+            pot = pot -1;
+            
+        }
+        System.out.println("El numero binario --" + binaryNumber + "-- en decimal es: " + suma);
+    }
 }
